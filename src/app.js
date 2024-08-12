13a2243c70d63453b233ecc5fa53f3b276a015ed
Rob <47312517+rmathew21@users.noreply.github.com>
2024-08-12 04:29:02 +0000

    added jobs.js file, and installed mustacheExpress

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const path = require('path');
+const JOBS = require('./jobs');
+const mustacheExpress = require('mustache-express');
+
+
 const app = express(); 
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.set('views', path.join(__dirname, 'pages'));
+app.set('view engine', 'mustache');
+app.engine('mustache', mustacheExpress());
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'pages/index.html'));
