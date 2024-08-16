6998b2b10bdd634a1ea2f582dc6a3ed68d0a11e1
Rob <47312517+rmathew21@users.noreply.github.com>
2024-08-16 05:17:05 +0000

    added post route for apply button, added transporter but needs fix

--- src/app.js
+++ src/app.js
@@ -31,17 +31,46 @@ app.get('/jobs:id', (req, res) => {
 })
 
 const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
+    host: 'smtp.google.com',
     port: 465,
-    secure: true,
+    
     auth: {
         user: process.env.EMAIL_ID,
-        pass: process.env.EMAIL_PASSWORD
+        pass: process.env.EMAIL_PASSWORD,
     }
 });
 
 app.post('/jobs/:id/apply', (req, res) => {
-    console.log(req.body);
+    const { name, email, phone, dob, coverletter } = req.body;
+    
+    const id = req.params.id;
+    const matchedJob = JOBS.find(job => job.id.toString() === id);
+
+    console.log('req.body', req.body);
+    console.log('matchedJob', matchedJob)
+
+    const mailOptions = {
+        from: process.env.EMAIL_ID,
+        to: process.env.EMAIL_ID,
+        subject: `New Application for ${matchedJob.title}`,
+        html: `
+            <p><strong>Name:</strong> ${name}</p>
+            <p><strong>Email:</strong> ${email}</p>
+            <p><strong>Phone:</strong> ${phone}</p>
+            <p><strong>Date of Birth:</strong> ${dob}</p>
+            <p><strong>Cover Letter:</strong> ${coverletter}</p>
+            `
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.error(error);
+            res.status(500).send('Error sending email');
+        } else {
+            console.log('Email sent: ' + info.response);
+            res.status(200).send('Email Sent successfully');
+        }
+    });
 });
 
 const port = process.env.PORT || 3000;
