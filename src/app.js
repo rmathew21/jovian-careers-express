745279c19ee680bad2d431022c13dd44b9d08517
rob <robmathew21@gmail.com>
2025-04-01 11:36:07 -0500

    changed company name on h1s from services to solutions, same change to footer, updated logo on all nav, updated link to Nav>Courses back to jovian

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ app.engine('mustache', mustacheExpress());
 
 app.get('/', (req, res) => {
     // res.sendFile(path.join(__dirname, 'pages/index.html'));
-    res.render('index', { jobs: JOBS, companyName: "Robo Web Services"});
+    res.render('index', { jobs: JOBS, companyName: "Robo Web Solutions"});
 });
 
 
